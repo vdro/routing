8e69fb06b17d41d6ded590d244adc5755f014b13
Tomasz Faldrowicz <Tomasz.faldrowicz@inkubit.com>
2019-06-09 11:01:52 +0200

    asd

--- routing-demo/src/app/department-detail/department-detail.component.ts
+++ routing-demo/src/app/department-detail/department-detail.component.ts
@@ -3,14 +3,8 @@ import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-department-detail',
   template: `
-    <h3>You selected department with id = {{departmentId}}</h3>
-    <p>
-      <button (click)="showOverview()">Overview</button>
-      <button (click)="showContact()">Contact</button>
-    </p>
-    
-    <router-outlet></router-outlet>
-
+    <h3>Wybrales Departament z id = {{departmentId}}</h3>
+   
     <p>
       <button (click)="goPrevious()">Previous</button>
       <button (click)="goNext()">Next</button>
@@ -51,11 +45,11 @@ export class DepartmentDetailComponent implements OnInit {
     this.router.navigate(['../', { id: selectedId }], { relativeTo: this.route });
   }
   
-  showOverview(){
-    this.router.navigate(['overview'], { relativeTo: this.route });
-  }
+  // showOverview(){
+  //   this.router.navigate(['overview'], { relativeTo: this.route });
+  // }
 
-  showContact(){
-    this.router.navigate(['contact'], { relativeTo: this.route });
-  }
+  // showContact(){
+  //   this.router.navigate(['contact'], { relativeTo: this.route });
+  // }
 }
